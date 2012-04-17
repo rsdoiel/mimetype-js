471e18a418c4decfef275c90f1e34f108686b77b
R. S. Doiel <rsdoiel@gmail.com>
2012-04-17 08:35:49 -0700

    Initial stub for mimetype_test.js

---
+++ mimetype_test.js
@@ -0,0 +1,18 @@
+//
+// mimetype_test.js - tests for mimetype.js module
+//
+// @author: R. S. Doiel, <rsdoiel@gmail.com>
+// copyright (c) 2012 all rights reserved
+//
+// Released under New the BSD License.
+// See: http://opensource.org/licenses/bsd-license.php
+//
+// revision: 0.0.0
+//
+
+var assert = require('assert'),
+  mimetype = require('./mimetype');
+
+console.log("Starting tests [mimetype.js] ...", new Date());
+assert.ok(false, "Tests Not Implemented yet.");
+console.log("Success!", new Date());
